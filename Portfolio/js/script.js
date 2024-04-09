68c2142e095c8708f18c61eb619b88d02d331ec0
KerKzek <166373836+KerKzek@users.noreply.github.com>
2024-04-09 08:54:52 +0700

    Delete Portfolio directory

--- Portfolio/js/script.js
+++
@@ -1,8 +0,0 @@
-// const as = document.getElementsByClassName("headbtn");
-// Array.from(as).forEach(element => {
-//     element.addEventListener("click",()=>{
-//         let ctent = element.textContent;
-//         element.textContent = 'Not Supported';
-//         setTimeout(()=>{element.textContent = ctent;},2000);
-//     });
-// });
