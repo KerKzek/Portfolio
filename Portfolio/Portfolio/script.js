c8a6bafebe5b7966c3400d7c62d649be704358fe
KerKzek <166373836+KerKzek@users.noreply.github.com>
2024-04-09 08:26:48 +0700

    Add files via upload

---
+++ Portfolio/Portfolio/script.js
@@ -0,0 +1,8 @@
+// const as = document.getElementsByClassName("headbtn");
+// Array.from(as).forEach(element => {
+//     element.addEventListener("click",()=>{
+//         let ctent = element.textContent;
+//         element.textContent = 'Not Supported';
+//         setTimeout(()=>{element.textContent = ctent;},2000);
+//     });
+// });
